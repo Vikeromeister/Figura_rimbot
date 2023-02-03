85a648f67252c9560f5872a6dd792252830e5f14
Vikeromeister <76005884+Vikeromeister@users.noreply.github.com>
2023-02-03 22:28:17 +0100

    Add files via upload

---
+++ szoveg.py
@@ -0,0 +1,724 @@
+sorok = []
+
+# 2 Kapufa
+sorok.extend("""
+Ha azt mondom, hogy kapufa lesz, kapufa lesz
+
+Ja, ja
+Tysonékkal itt lent
+Téren nyomjuk ami kell
+Mában minden ami kell
+Csepereg a csermely
+Garázsban folyik a szesz
+Itt Vacsora lesz
+Itt ma csuma lesz
+Ma valaki apuka lesz
+Ebből ma kapufa lesz
+
+Az illatom Boss
+A tiéd az rossz
+Iszod a Voss-t
+Én eszem a koszt
+Tyson a boss
+Látod a gyereket
+Let's get lost
+Te hülye fosz
+
+Squadom az Batman
+Te ezt sosem adod be
+Falusi Pac-man
+Te szarod sosem eszem meg
+Gieroy gang gang
+Napom most kezdődik el
+A Téren legendák születnek
+Amit sosem hiszel el, itt
+Sülnek a kiflik
+Zsebemen tíz-tíz
+Oh Istenem, plíz plíz
+Lever a víz, víz
+A zsebemen tíz-tíz
+Te meg csak hisztizz
+Kecsenye tíz-tíz
+Pörög a friss liszt
+Látod a gyereked
+""".splitlines())
+
+# 3 Nutella
+sorok.extend("""
+Olyan zöld a szarom, hogy már brekegek
+Ja, ja
+Ja, faszom tele veletek
+Haszontalan gyerekek
+Hív a glxy én lemegyek
+Ja a track-ek itt melegek
+Vágja a falu a Figuék betegek
+Én csak hajszolom a kenyeret
+Ez csak friss fű, amit legelek
+Ez egy rossz lány akit bekenek
+Fogd a kormányt amíg tekerek
+Itt van a fő boss, jönnek a levelek
+Süti a pék, nézd milyet keverek
+Mert én egész nap csak szerelek
+Folyik a szósz a lányból, ez meredek
+Mint a Bibliában megjelenek
+Minden vérem, testem itt van, egyetek
+Benned már egy busz megállhat
+Leszopod a Nutellámat
+Leszarom az új verdádat
+Olyan forró ez a klub
+Lehet haza se megyek
+Köszönöm az Istennek, hogy fasza lehetek
+Köszönöm, hogy laza lehetek
+Nálam van a jackpot
+De be se emelek
+Legfiatalabb Ken Block
+De bele se megyek
+Faszom tele veletek
+Haszontalan gyerekek
+Hív a glxy, én lemegyek
+Ja a track-ek itt melegek
+Vágja a falu a Figuék betegek
+Olyan zöld a szarom, hogy már brekegek
+Én csak hajszolom a kenyeret
+Ez csak friss fű, amit legelek
+Csak iszom a Gieroy-t
+Reggel az erdőben kelek
+Ránézek az órára:
+2:07, ez remek (Nice)
+Tysonékkal téren nyomjuk ami kell
+Mindenünk van, mindenünk van ami kell
+""".splitlines())
+
+# 4 Zsé
+sorok.extend("""
+Glxy a beaten, lázas negyven fok
+Én csak keresek valamit, je
+E-cigi
+
+Hol van a zsém?
+
+Kérdezik, hol van a zsém?
+Én is tudni szeretném
+Folyik a szóda, egy-hét
+Folyik a vodka, ezt nézd
+Minden emberem egy gép
+Ezek reggeliznek egy gét
+Süti a pék
+Itt van a krém
+Figu ez kettő-nulla-hét
+Én csak tudni szeretném
+
+Mondd, hol van a zsém?
+
+Megvan a tegnap képekben
+Gieroy folyt le a szőnyegre
+Térjünk légyszi a lényegre
+Nem nézek a fékekre, meg a kékekre
+Kortyolok erre a méregre
+Sok van, nem mérem le
+Ragyogok, mint az ékszerek
+Vagy csak képzeled
+Kutatom mint a pénzemet
+Meg a két szemed
+Kutatom mint a pénzemet
+Meg a két szemed
+
+551, 27 amit mi rakunk az ettől szép
+""".splitlines())
+
+# 5 Finoma
+sorok.extend("""
+Semmi zaj, semmi baj
+Ki ez a csaj, hey
+Nincs vele baj
+Szereti, ha finoma, ja
+
+Okkal vagyok itt
+Lányokkal vagyok itt
+Nem tudom miért vagyok itt
+De okkal vagyok itt
+Mi volt tegnap az mítosz
+Nem bánom a holnap mit hoz
+De a mai még megkínoz
+
+Ja, én meg a crew
+Tele a klub
+Csajod egy kú-
+Izzad a crew
+Figura blúz
+Fele se plusz
+Veszek neked McFreeze-t
+De nem akarom, hogy meghízz
+Csak azt, hogy bennem megbízz
+Csak azt, hogy bennem meg
+""".splitlines())
+
+# 6 Hétfő
+sorok.extend("""
+Hogyha hétfő van akkor megismerlek
+Hogyha szerda van akkor elfelejtelek
+Hogyha péntek van talán meg is szeretlek
+Kivéve csütörtök, kivéve csütörtök
+Hogyha hétfő van akkor megismerlek
+Hogyha szerda van akkor elfelejtelek
+Hogyha péntek van talán meg is szeretlek
+Kivéve csütörtök, kivéve csütörtök, ah
+
+Tudod, hogy rád várok
+Tőled lesz gyerekem
+Meg veszem az Árkádot
+Mert mindenen átlátok
+Tőled lesz gyerekem
+Sohanem etetem, ja
+Remélem még leszel a barátom
+Remélem még leszel
+Remélem hogy leszel
+
+Csak folyik le a pénz
+De mond, minek is élsz
+Hogy fogalmad sincs
+Csak fújja a szél és
+Senki nem beszél
+Mert fogalmuk sincs
+Tudod a város cool
+Minden emberem cool
+Nem kéne, hogy félj
+Velem kéne indulj
+Olyan jó most a fény
+Nem kéne izgulj
+Poharadban egy Gina
+Nemtudom hogy ki volt
+""".splitlines())
+
+# 7 Csermely
+
+sorok.extend("""
+Vissza, vissza
+Mindenki csermelyem issza
+
+Semmit nem fordíthatok vissza
+Mindenki csermelyem issza
+Azért, mert annyira tiszta
+Mint LL Juni ez is gizda
+Csak folyok le a hegyekről
+Mint egy tiszta csermely
+14:07, itt az idő ember (2-7)
+Annyit aludtam
+Hogy azt hittem nem kelek fel
+Kettőt pislogok és van egy gyerekem
+Semmit nem fordíthatok vissza
+Mindenki csermelyem issza
+Azért, mert annyira tiszta
+Mint LL Juni ez is gizda
+Csak folyok le a hegyekről
+Mint egy tiszta csermely
+Ezt az éjszakát soha nem felejtem el
+
+Sokan vannak, hosszú a lista
+Mindenki a csermelyem issza
+Itt van a szívem kulcsa
+Majd ha megunod dobd csak vissza
+Itt van a Figura a pályán
+Tele van loottal a ládám
+Tanulok másnak a kárán
+Gyorsan mozgok, mint a Neymar
+Mennem kell hívott a pályám
+Ma ne várj rám
+De te miért vagy annyit a rúdon?
+Mikor rám jár
+Egész délelőtt alszom, ez a márkám
+Egész éjjel dolgozok egy lánynál
+
+Mindenki csermelyem wou
+""".splitlines())
+
+# 8 Sasuke
+sorok.extend("""
+Ja, Sasuke, arigato
+Én futok, ti lemaradtok
+Futok, ti lemaradtok
+Nem marok, Ti bemarattok
+Sasuke, arigato
+Futok, lemaradtok
+Bármikor bemarattok
+Pedig nem is marok
+Bármikor bemarattok
+Mert ez itt Hungária
+Szét kell pumpálnia
+Alpár a gang
+Mint Románia
+Rommá bio
+Rommá pia
+Jézus fiai ez kompánia
+Ez egy Putto fia, egy uccsó pia
+Még bruttó giga
+Ez egy utópia
+Látom a jövőt és egyre jobb
+Glxy a beaten, 40 fok
+Harap a krokodilom, meg ne fogd
+Hazamentem, mert kedvem volt
+Tegnap a BaleeB rendben volt
+Nem vágja a baligát
+Meggátol a baligát
+Soha nem kérek karikát
+Karikát, öt karika olimpiai Baliba
+Ezt kérem karira
+Hogy Lemehessek Balira
+Fénylik a szarom, mint a Győzike Ferrarija
+Vérzik a zsebem, de már érzik a szavamon, hogy ez mágia
+Megjelenek a bulitokon, velem van a Szűz Mária
+Mindenképp lighty-t rakunk
+Tysonnal rommá vagyunk
+Minden napunk ilyen, minden nap
+Kezembe porrá hamu
+Nincsenek korlátok
+Fogom a kormányod
+Ha fény van ott jól látod
+Iszom a Cola-tok
+Elszívom a smokkátok
+Hívjatok tolmácsot
+""".splitlines())
+
+# 9 Ahha
+
+sorok.extend("""
+Nálatok alszom egy pár napot
+Ahha, ahha, ahha
+Az arcodban párna, ott
+Elűzöm a bánatot
+Felhívom a Bálintot
+
+A, anyukád rámnyitott, aha
+A, húgod is rámnyitott, aha
+A, hozod a bornyitót, aha
+A, az a lány úgy ordított, hogy:
+
+Figura bullshit
+Csapkodom a husit
+A csajod az pucsít
+Elkenem a rúzst
+
+Új set
+Gizda vagyok mint egy pejló
+Holdról megy a Vénuszra a metróm
+Készpénz, az hogy honnan van azt nemtom
+Válts vissza, ha nem bírod a tempóm
+Sok a hívő, tele van a templom
+Kifötör a kerék, fiatal Ken Block
+Hogyha nem bírok vele jön a bang-bro
+Neked bűzlik a szarod, itt egy Negro
+""".splitlines())
+
+# 10 HSP
+
+sorok.extend("""
+Én nem akarok híres lenni
+Csak veled együtt boldog lenni
+Már nem izgat a pénz sem, tényleg
+Csak nem akarok rottyon lenni
+Barátom is messze tőlem
+Családom is messze tőlem
+Túl messze jöttem
+
+Nekem nem kell a pénz haver
+Inkább mutasd meg, hogy élsz haver
+Nekem hiányzik a tér, haver
+Ahogy közeleg a tél, haver
+Pacsi azoknak akik változnak
+Pacsi azoknak akik átkoznak
+Az idő utolér mindenkit
+Pacsi a véremnek, a Tysonnak
+Sokan idő előtt távoznak
+Sokan a múltjukon rágódnak
+Sokan a jövőjükön stresszelnek
+De te itt vagy velem a jelenben
+Nem bánom azt amit megtettem
+Volt ami volt, én már ez lettem
+A döntéseim miatt szenvedtem
+Nyerni akartam de vesztettem
+
+De ahol becsukódnak ajtók ott újak nyilnak
+Akkor már nem érdekel semmi, ha újra itt vagy
+Most még velem van a remény, de majd újra itt hagy
+Semmi nem tart örökké, de ezt te is tudtad
+Hiányzik a nyár, amikor nyomtuk a tetőn
+Sok van a hátunk mögött, de kevés az orrunk előtt
+Van ami nem változik, akkor se ha a fejünk benő
+Van amiket csak a Gieroy hozhat elő
+Tesó mi ugyan ott kezdtük ahol, majd befejezzük
+Ott kezdtük ahol, majd befejezzük
+Ott kezdtük ahol, majd befejezzük
+De ott kezdtük ahol, majd befejezzük
+Csak tettük amit akartunk és szabadok voltunk
+Ittuk a Freeway-ünket és boldogok voltunk
+Semmi nem a régi már, ja
+De ne mondd, hogy az én hibám
+""".splitlines())
+
+# 11 Oh no
+sorok.extend("""
+Oh no
+
+Nézd meg a csajt, haver ez forró
+Csak iszom a Sprite-Gieroy kombót
+Csak simogat a szél, öreg ez pont jó
+Öreg ez pont jó
+Légyszi rázd meg a popót, slowmo
+
+Nem férsz bele a körbe, sumo
+Plusz kettőre a 07, UNO
+Hopp, behúztalak bőrbe, oh no
+Nem tudom, de tele a zsák
+Ne csak nézz, kicsit láss
+Ja, ez kicsit más
+Ez magic, kuruzslás
+Nem tudom, hogy kiben bízzak meg
+Nem tudom
+Hogy mennyit leszek lent
+Nem tudom, nem tudom
+
+Tedd rám a popót
+Bebe Oh slowmo
+Tedd rám a popót
+""".splitlines())
+
+# 12 Puma
+sorok.extend("""
+Ma is megint, csuma csuma csuma
+Haza futok mint egy puma, puma, puma
+Ahogy feszül az a ruha, ruha, ruha
+Szájában a Hubba Bubba, Bubba
+Cselekedjünk, sok a duma, duma
+Táncolj nekem, megy a ruma, ruma, ruma
+Álj, ez finom, folyik mint a Duna, Duna
+De haza futok mint egy puma, puma
+
+Újat kever a pék
+Olyat sütök, megint lever a víz
+Azt hiszed, hogy ez itt Miami Beach
+Figu haverom, itt fröcsög a liszt
+Nem Miami Beach
+Figu haverom, itt fröcsög a liszt
+Nem Miami Beach
+Kecsenye boy
+Ez a flow at least
+Rakja a beatre G, Ben 10, hi hi
+Keresem, a cash-em a távcsőmmel
+Keresem, a cash-em a távcsőmmel (hi hi)
+Baby átjössz akkor bend over
+Nagy segg, mint egy Land Rover
+Pénz az nincs, ez egy broke brooker
+Nekem a lisztem friss
+Neked pornó kell
+""".splitlines())
+
+# 13 40
+sorok.extend("""
+Lázas, 40 fok
+Glxy a beaten, lázas 40 fok
+Csak egy kicsikét lejjebb fogd
+Csak egy kicsikét lejjebb fogd, lejjebb fogd
+
+Nem zavar, hogy tegnap mi is volt
+Kérlek baby, szeress most, szeress most
+A beaten 40 fok, kicsikét lejjebb fogd, csak kicsikét lejjebb fogd
+Kicsit csuma van
+De engem nem zavar
+Egy kicsit csuma van
+De ez a kisebb baj
+
+Engem nem zavar, hogy téged más hajt
+De téged ne zavarjon engem mi hajt
+Nekem annyi kell, hogy legyen itt 1 csaj
+Aki tudja mi velem a baj, ha van
+Magamban sem bízok megy a taliga
+Ha így folytatom nem jutok le Balira
+Átlátszóak vagytok, mint egy karika
+Alattam meg sprintel ez a paripa
+""".splitlines())
+
+# 14 Johnny Test
+sorok.extend("""
+Jah, jah, jah, jah, jah
+Csak repülök el
+Mint Johnny Test
+Maradok lent
+Ahogy azt kell
+Repülök el
+Mint Johnny Test
+Megyek most szét
+Most leveszed azt
+Én meg leveszem ezt
+Most leszopod azt
+Meg beszopod ezt
+És is beszopom azt, ja
+Te is beszopod ezt
+Bedobod azt
+Közösbe azt,ja
+
+Bedobod ezt
+Bedobom azt
+Bedobom ezt, ja, ja
+Ezek itt sohanem alszanak
+Soha nem alszanak
+""".splitlines())
+
+# 15 OMG
+sorok.extend("""Azt sem tudom miért vagyok itt
+Most már tudom miért vagyok itt
+Azt sem tudom miért vagyok itt
+Most már tudom miért vagyok itt
+Hasítunk, 2-0-7 kiló minimum, minimum
+OMG, nem kérek én sokat, csak egy modell lányt
+Istenem, Oh My God
+Kérlek szépen fogadj rám
+Felkelek és üldöz már a pék
+De szart hidd el, sosem árulnék
+De szart hidd el sosem sütnék
+Mert tudom merre van az ösvény
+Tsubasatól kapod a kötényt
+Robertotól kapod a kötvényt
+Tsubasatól kapod a kötényt
+Én csak pihenek itthon a kanapén
+Need For Speed, életveszély
+Ez Colin McRae, speedrace, rally
+Füstöl a kaszni, ma is megint gatya
+Csajnak mi a baja, nemtom mi a baja
+Folyik mint a Niagara""".splitlines())
+
+# 16 Crisps
+sorok.extend("""
+Hertelendi tigris
+
+Az ég a limit
+Kutya meg visít
+Rúgom a bőrt
+Bokám eltörik
+Gierotty pörög
+
+De nem vagyok török
+Finom a gírosz
+De nem vagyok görög
+Finom a szád
+A gierotty pörög
+Tele a ház
+Mennek a körök
+Nem is látsz
+A bokorból jövök
+Vigyázz, mert lövök
+
+Nem is nézek rád
+Tudod, hogy adom
+Vadászok vacsorát
+A kínait tudom, hogy unod
+Nem megyek ma el te rendelsz
+Elég nekem, hogyha bekedvelsz
+Vagy benedvelsz
+Verem a vasat, az ereszen folyik a vaj
+Tele a zsebem, kipotyog a szar
+Folyik a nyakam, a testem az fatal error
+Mint a Figura garázs, hogyha tele a lakás
+Akkor az parás, mint a terrorház
+Tsubasa gang, kirúgom a parázst
+Olyan finom volt a kalács
+Csak egy nyalás
+Olyan finom volt a kalács
+Csak egy nyalás
+Az én valutám, sosem rakom rád
+Azért vagyok sky, hogy sétáljatok rám
+Azért vagyok high, mert ti nagyon lájti
+Chill zone, nem kell semmi, fight itt bro
+Chill bro, itt egy kis chips bro
+Ja az cool, bro
+Mesélj még, bro
+Ja az cool, bro
+
+Igazi lakodalom
+A pinák jobbra húzva
+A gang a bal oldalon
+A pinák csak halak a Balatonon
+Csámcsogok itt a falatokon
+2-0-7 a falatokon
+Rakatok rakatokon
+Keret a fejeteken
+Maszk a fejeteken
+
+Sok Task
+Sok Crisp
+Csajod nyomja itt a furulyát
+De tudom, hogy csak vicc
+Tudom, hogy csak vicc
+""".splitlines())
+
+# 17 Meredek
+sorok.extend("""
+A, annyira maradok lent
+Pont rám ismerjen a gang
+Falum ott a térképen
+Iszunk, de nem mértékkel
+Baranya, fenegyerek
+Nem vágják itt a nevemet
+Baranya fenegyerek
+Az enyém akkor is meredekebb
+Meredek, ja
+Meredek, ja, ja
+""".splitlines())
+
+# 18 Pizsi
+sorok.extend("""Olyan otthonossan mozgok itt a city-ben
+Ott vagyok az üléseken is
+Meg minden riddim-en
+Stresszelek a heteken
+Ez week idegJa, a 27-es jéghideg
+Erre verik ki a pingvinek
+Erre fenik a foguk a fókák
+Ugorjatok át, ha kívánod a mókát
+High five, hogyha kivárod a pályát
+Hárfán tolom, imádom a lábát
+High five, hogyha kivárod a pályát
+Hárfán tolom, imádom a lábát
+Elnézését kérem, de megkívántam a lányát
+Szívesen megágyaznám az ágyát
+Veszek neki pénzt, meg minden ami drágát
+Az alakod is jó, de amúgy rádhívtam a NASA-t
+A csuklóm hideg, mint a fridzsider
+Rámnyitott a húgod, de csak feküdtem ott pizsiben
+Nem tehetek róla
+A csajod engem kifigyelt, Te miket irogatsz
+Mindjárt tiltlak az iWiW-en
+Olyat sütök, hogy a maffia is kifizet, ah
+Nem maradtak a városban már civilek, ah
+Teleirogatom megint füzetet, ah
+2-0-7, ez az övezet""".splitlines())
+
+# 19 Pápa
+sorok.extend("""
+Kicsit pislákol, én megint lángolok
+Gyere kislány én neked táncolok
+
+Potyog itt a hegyeken a szőlő meg a stex
+Papa bora, apa húga, te ezt sosem adod be
+Gangem az aljas kúszóbab, aljas kúszóbab
+Long az island, meg long a pénzem
+Lóg az ablakban a gatya, feszülök a téren
+Minden napom plusz minden napom péntek
+Tolom mint a Bluest, de tényleg
+Ki ez a bitch? Ki ez a bitch?
+Úgy nézel ki, mint a grincs
+24/24 elsüllyedtem, de úszok még
+Gangemen a 27, csak simogat a szél
+Gyere ma is essünk szét, ez az álom olyan szép
+Ez az álom olyan szép
+Reggel van 6, jön fel a Nap
+Ki vagyok, be vagyok, nálam a víz, kéne egy pap
+Hívogatnak, hívogatnak
+Neked a láz, nekem a tűz, bele ne állj, bele ne ülj
+Izom a kraft, tele a pakk, söci ablak
+Nem kell a tek-tek-tek, veres a blant
+Tele a ház, Figura gyász
+Ja az klassz, mesélj még
+Ja az klassz, mesélj még nekem
+Ja az klassz, ja az klassz
+Ja az klassz
+""".splitlines())
+
+# 20 Offbeat
+
+sorok.extend("""
+Ja, a szívem olyan off beat, mint a testem
+Ja, a nyakamon a bling bling minden este
+Ja, a csapatom az Kill Bill, minden ember
+Ez friss liszt
+27 a szívem, ember
+Ember, minden rendőr itt az utcába' kempel
+Te csak kelj fel, hívtam kocsit
+Te csak, menj el
+Fölkelek és már, kell, kell nekem új
+Kell, kell nekem új
+
+Mikor a sarokra már látod, ahogy kanyarodok
+Én mindent haza hozok
+Sosem pazarolok
+Ez a Figu egy fasza kölyök, fasza kölyök
+Mindenki flexel, mindenki stresszel
+Csak töltöm poharamat tele, mint a Presser
+Mindenki beefel, mindenki flessel
+Ahhoz túl jó a punci, hogy elfelejtsem
+
+Én állok a lasztin, állsz
+És jön a galaxy-nál
+Ez falusi fesztivál
+Te maradj a masztinál
+Csajom az praktizál
+A tied az baszni jár
+Figu egy partizán
+""".splitlines())
+
+# Fala Fel
+sorok.extend("""
+Glxy a beaten lázas 40 fok
+Keyworld
+Találkozunk félkor
+Ecigi
+
+Találkozzunk félkor a hely előtt
+Gyere velem félre
+Már barátkoztunk ezelőtt
+Szóval ne legyél most szem előtt
+Benézek a helyre és csak kapkodod a levegőt
+Anyukád egy pornósztár
+És maga után vasalja a lepedőt
+Mutasd hogy mit hoztál
+Alád betolom az emelőt
+
+Rázzuk meg együtt a popsit
+Fiatal figura pop shit
+Soha nem cserélek aksit
+Reggelig rugdosom a lasztit
+
+Mi a baj mondd mi a para?
+Kaja kell?
+Itt a sarkon van a legfinomabb falafel
+Vagy nyomjalak a falra fel
+Tele van a hasad meg a szád
+Akkor megolvad a karamell
+Segg fel, nózi le
+Olyan finom ez a kacsamell
+Kenegetem be a sütőbe a besamellt
+Kaja fétis, minden emberem egy chef
+Kóstolom a bort
+Senki sem ért
+Át utazom az EU-t is a barátnőmért
+Nem szeretem a vért
+Csak akkor hogyha folyik el
+A gyerekeimért
+Remélem, hogy nem az enyém
+Az a kisfiú ott melletted
+Nem szerepelt a tervemben
+De elviszlek a szigetekre
+Nincs is olyan messze
+Homokot szórok a seggedre
+Csinálok majd képet a Faced-re
+De máshova nem
+Mert oda nem engedem
+Nem engedem
+""".splitlines())
+
+# Jégkori
+sorok.extend("""
+Hi! Itt van a báttya
+Csúszik a pálya
+Jó napot uram, az van
+Hogy tetszik a lánya
+Elvinném Ibizára
+De ott nincsen jégkori pálya
+Pampi a mába
+Egsi a mába
+
+Forrócsokizunk a pályán
+Forró a csaj mint a kályhám
+Figura a pályán
+Mindenki rám vár
+Jégkori pályán pörgünk
+Pörgünk
+""".splitlines())
